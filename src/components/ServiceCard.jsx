8ed791c63fb732f66ac35a362c88cdad2ce6c4df
Foysal Hossien <foysalhossien77@gmail.com>
2025-08-16 00:13:10 +0600

    complete ourWork section

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -27,7 +27,7 @@ const ServiceCard = ({ service, index }) => {
         style={{ top: position.y - 150, left: position.x - 150 }}
       />
 
-      <div className="flex items-center gap-10 p-8 hover:p-7.5 hover:m-0.5 transition-all rounded-[10px] bg-white dark:bg-gray-900 z-10 relative">
+      <div className="flex items-center gap-10 p-8 hover:p-7.5 hover:m-0.5 transition-all cursor-pointer rounded-[10px] bg-white dark:bg-gray-900 z-10 relative">
         <div className="bg-gray-100 dark:bg-gray-700 rounded-full">
           <img
             src={service.icon}
