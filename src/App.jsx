0f4fa5bcfa398e28eb1ed8d5ad8bb785660dab2c
Foysal Hossien <foysalhossien77@gmail.com>
2025-08-15 22:00:24 +0600

    Complete trustedby section

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import Hero from "./components/Hero";
 import TrustedBy from "./components/TrustedBy";
 
 const App = () => {
-  const [theme, setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light');
+  const [theme, setTheme] = useState(
+    localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
+  );
 
   return (
     <div className="dark:bg-black relative">
-      <Navbar theme={theme} setTheme={setTheme}/>
+      <Navbar theme={theme} setTheme={setTheme} />
       <Hero />
-      <TrustedBy/>
+      <TrustedBy />
     </div>
   );
 };
